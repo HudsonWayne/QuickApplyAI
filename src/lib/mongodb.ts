ac67a4581b6f0ccb5014fa3e7219577349ebc04e
HudsonWayne <waynewaynebenhura@gmail.com>
2025-08-07 09:42:45 +0200

    initial commit

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,19 +1,13 @@
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI!;
-const options = {};
-let client;
-let clientPromise;
-
-if (!process.env.MONGODB_URI) {
-  throw new Error("Please add your Mongo URI to .env.local");
-}
-
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+const client = new MongoClient(uri);
+const dbName = "quickapplyai";
 
 export async function connectToDatabase() {
-  const client = await clientPromise;
-  const db = client.db("quickapplyai");
-  return { db };
+  if (!client.topology || !client.topology.isConnected()) {
+    await client.connect();
+  }
+  const db = client.db(dbName);
+  return { client, db };
 }
